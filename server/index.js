a25043623cef63a63787d3fb140c2a45c595474f
David Perelman <dperelman3@gmail.com>
2022-05-24 10:44:48 +0300

    Connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/test');
+  await mongoose.connect(process.env.MDB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+      console.log('Connected to MongoDB');
+  });
 }
 
 app.use(bodyParser.urlencoded({ extended: false }))
