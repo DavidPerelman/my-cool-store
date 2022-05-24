56a45d23a4f6351051e482ec93865496535ed2e4
David Perelman <dperelman3@gmail.com>
2022-05-24 11:04:48 +0300

    userModel completed

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require('moment-timezone');
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -16,6 +17,11 @@ const userSchema = new mongoose.Schema({
       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   },
   passwordHash: { type: String, required: true },
+  registerDate: {
+    type: String,
+    required: true,
+    default: () => moment().format('DD/MM/YYYY hh:mm'),
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);
