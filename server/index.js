3af40b379b5d0a1f0483f5092528f386cb3a36b4
David Perelman <dperelman3@gmail.com>
2022-05-24 10:41:24 +0300

    setup dotenv

--- server/index.js
+++ server/index.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+}
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/register', async (req, res) => {
+    const data = { firstName, lastName, email, password } = req.body;
+    console.log(data);
+})
 
 app.listen(3000, () => {
     console.log(`Server running`)
