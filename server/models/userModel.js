18883b89ad85d78c9d04789b9ab41ae151005919
David Perelman <dperelman3@gmail.com>
2022-05-24 11:02:12 +0300

    setup Moment.js library

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  
+  firstName: {
+      type: String,
+      required: true
+  },
+  lastName: {
+      type: String,
+      required: true
+  },
+  email: {
+      type: String,
+      required: true,
+      match:
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  },
+  passwordHash: { type: String, required: true },
 });
 
 module.exports = mongoose.model('User', userSchema);
