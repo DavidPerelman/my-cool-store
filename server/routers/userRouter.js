033636c75e2af1ec7f0b315788fc7dc2262b08c8
David Perelman <dperelman3@gmail.com>
2022-05-24 11:43:01 +0300

    existingUser completed

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -2,8 +2,20 @@ const router = require('express').Router();
 const User = require('../models/userModel');
 
 router.post('/register', async (req, res) => {
-    const data = { firstName, lastName, email, password } = req.body;
-    console.log(data);
-})
+    try {
+         const data = { firstName, lastName, email, password } = req.body;
+        console.log(data);
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({
+                errMessage: 'An account with this email already exist.',
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
 
 module.exports = router;
