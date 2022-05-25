842aa492e7f5ba17120147a78bbf803530e642c3
David Perelman <dperelman3@gmail.com>
2022-05-25 22:28:17 +0300

    setup jsonwebtoken

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/userModel');
+const Token = require('../models/tokenModel');
 const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res) => {
