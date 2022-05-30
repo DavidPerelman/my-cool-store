a6ed614250f2f8de7e9de4ecccd4c83941a5d653
David Perelman <dperelman3@gmail.com>
2022-05-30 20:45:39 +0300

    refactor

--- server/index.js
+++ server/index.js
@@ -4,18 +4,22 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config()
+dotenv.config();
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.MDB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  await mongoose
+    .connect(process.env.MDB_CONNECT, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
       console.log('Connected to MongoDB');
-  });
+    });
 }
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(express.json());
 
 // Routers
@@ -23,5 +27,5 @@ const userRouter = require('./routers/userRouter');
 app.use('/user', userRouter);
 
 app.listen(3000, () => {
-    console.log(`Server running`)
+  console.log(`Server running`);
 });
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,4 +1,4 @@
-const sendEmail = require("../utils/email");
+const sendEmail = require('../utils/email');
 const router = require('express').Router();
 const User = require('../models/userModel');
 const Token = require('../models/tokenModel');
@@ -6,68 +6,68 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
-    try {
-         const data = { firstName, lastName, email, password } = req.body;
+  try {
+    const data = ({ firstName, lastName, email, password } = req.body);
 
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({
-                errMessage: 'An account with this email already exist.',
-            });
-        }
-
-        // hash the password
-        const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password, salt);
-
-        // create a new user
-        const newUser = await new User({
-            firstName,
-            lastName,
-            email,
-            passwordHash,
-        }).save();
-
-        const token = await new Token({
-            userId: newUser._id,
-            token: jwt.sign(
-            {
-                user: newUser._id,
-            },
-            process.env.JWT_SECRET
-        )
-        }).save();
-
-        const message = `${process.env.BASE_URL}/verify/${newUser._id}/${token.token}`;
-        await sendEmail(newUser.email, "Verify Email", message);
-        console.log(newUser.email);
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).send();
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        errMessage: 'An account with this email already exist.',
+      });
     }
+
+    // hash the password
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // create a new user
+    const newUser = await new User({
+      firstName,
+      lastName,
+      email,
+      passwordHash,
+    }).save();
+
+    const token = await new Token({
+      userId: newUser._id,
+      token: jwt.sign(
+        {
+          user: newUser._id,
+        },
+        process.env.JWT_SECRET
+      ),
+    }).save();
+
+    const message = `${process.env.BASE_URL}/verify/${newUser._id}/${token.token}`;
+    await sendEmail(newUser.email, 'Verify Email', message);
+    console.log(newUser.email);
+    res.json('ok');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
 });
 
 router.get('/verify/:id/:token', async (req, res) => {
-    try {
-        console.log(req.params.id);
-        const user = await User.findOne({ _id: req.params.id });
-        if (!user) return res.status(400).send("Invalid link");
+  try {
+    console.log(req.params.id);
+    const user = await User.findOne({ _id: req.params.id });
+    if (!user) return res.status(400).send('Invalid link');
 
-        const token = await Token.findOne({ 
-            userId: user._id,
-            token: req.params.token 
-        });
-        if (!token) return res.status(400).send("Invalid link");
+    const token = await Token.findOne({
+      userId: user._id,
+      token: req.params.token,
+    });
+    if (!token) return res.status(400).send('Invalid link');
 
-        console.log(token);
-        await User.updateOne({ _id: user._id, verified: true });
-        await Token.findByIdAndRemove(token._id);
+    console.log(token);
+    await User.updateOne({ _id: user._id, verified: true });
+    await Token.findByIdAndRemove(token._id);
 
-        res.send("email verified sucessfully");
-    } catch (err) {
-        res.status(400).send("An error occured");
-    }
-})
+    res.send('email verified sucessfully');
+  } catch (err) {
+    res.status(400).send('An error occured');
+  }
+});
 
 module.exports = router;
