b4576f309051524fd9d32942283ca770d21b219f
David Perelman <dperelman3@gmail.com>
2022-06-07 23:59:32 +0300

    RegisterModal 1

--- client/src/components/RegisterModal.js
+++ client/src/components/RegisterModal.js
@@ -9,11 +9,6 @@ import axios from 'axios';
 
 const RegisterModal = ({ show, onClose, registered, setRegistered }) => {
   const [error, setError] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [verifyPassword, setVerifyPassword] = useState('');
 
   const [registerData, setRegisterData] = useState({
     firstName: '',
@@ -23,28 +18,26 @@ const RegisterModal = ({ show, onClose, registered, setRegistered }) => {
     verifyPassword: '',
   }); // new way etgar
 
-  console.log(registerData.firstName);
-
   const register = async (e) => {
     e.preventDefault();
 
-    const registerData = {
-      firstName,
-      lastName,
-      email,
-      password,
-      verifyPassword,
-    };
-
     if (
-      isFormFieldsValid(firstName, lastName, email, password, verifyPassword)
+      isFormFieldsValid(
+        registerData.firstName,
+        registerData.lastName,
+        registerData.email,
+        registerData.password,
+        registerData.verifyPassword
+      )
     ) {
       setError('all fields required!');
-    } else if (isValidPassword(password, verifyPassword)) {
+    } else if (
+      isValidPassword(registerData.password, registerData.verifyPassword)
+    ) {
       setError(
         'Password must be at least 6 characters long and the passwords must be identical'
       );
-    } else if (!isValidEmail(email)) {
+    } else if (!isValidEmail(registerData.email)) {
       setError('invalid email!');
     } else {
       setError('');
@@ -52,7 +45,6 @@ const RegisterModal = ({ show, onClose, registered, setRegistered }) => {
         let res = await axios.post('/user/register', registerData);
         setRegistered(true);
         clearFormFields();
-        console.log(res);
       } catch (err) {
         console.log(err.response.data.errMessage);
         setError(err.response.data.errMessage);
@@ -66,30 +58,21 @@ const RegisterModal = ({ show, onClose, registered, setRegistered }) => {
     }
   };
 
-  const registerForm = () => {
-    setRegistered(true);
-  };
-
   const closeModal = () => {
     onClose();
     clearFormFields();
   };
 
   const clearFormFields = () => {
-    for (const property in registerData) {
-      registerData[property] = '';
+    for (let field in registerData) {
+      registerData[field] = '';
     }
-    // setFirstName('');
-    // setLastName('');
-    // setEmail('');
-    // setPassword('');
-    // setVerifyPassword('');
   };
 
-  const handleFormChange = (e) => {
+  const handleFormChange = (e, valKey) => {
     const { value } = e.target;
     setRegisterData((prevState) => {
-      return { ...prevState, firstName: value };
+      return { ...prevState, [valKey]: value };
     });
   };
 
@@ -125,73 +108,26 @@ const RegisterModal = ({ show, onClose, registered, setRegistered }) => {
             )) ||
               ''}
             <form>
-              <div className='mb-3'>
-                <label htmlFor='exampleInputFirstName1' className='form-label'>
-                  First Name
-                </label>
-                <input
-                  type='text'
-                  className='form-control'
-                  id='exampleInputFirstName1'
-                  aria-describedby='firstNameHelp'
-                  onChange={handleFormChange}
-                  // onChange={(e) => setFirstName(e.target.value)}
-                  value={registerData.firstName}
-                />
-              </div>
-              <div className='mb-3'>
-                <label htmlFor='exampleInputLastName1' className='form-label'>
-                  Last Name
-                </label>
-                <input
-                  type='text'
-                  className='form-control'
-                  id='exampleInputLastName1'
-                  aria-describedby='lastNameHelp'
-                  onChange={(e) => setLastName(e.target.value)}
-                  value={lastName}
-                />
-              </div>
-              <div className='mb-3'>
-                <label htmlFor='exampleInputEmail1' className='form-label'>
-                  Email
-                </label>
-                <input
-                  type='email'
-                  className='form-control'
-                  id='exampleInputEmail1'
-                  aria-describedby='emailHelp'
-                  onChange={(e) => setEmail(e.target.value)}
-                  value={email}
-                />
-              </div>
-              <div className='mb-3'>
-                <label htmlFor='exampleInputPassword1' className='form-label'>
-                  Password
-                </label>
-                <input
-                  type='password'
-                  className='form-control'
-                  id='exampleInputPassword1'
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
-                />
-              </div>
-              <div className='mb-3'>
-                <label
-                  htmlFor='exampleInputVerifyPassword1'
-                  className='form-label'
-                >
-                  Verify Password
-                </label>
-                <input
-                  type='password'
-                  className='form-control'
-                  id='exampleInputVerifyPassword1'
-                  onChange={(e) => setVerifyPassword(e.target.value)}
-                  value={verifyPassword}
-                />
-              </div>
+              {Object.keys(registerData).map((field, i) => {
+                return (
+                  <div key={i}>
+                    <label className='form-label'>
+                      {field.charAt(0).toUpperCase() +
+                        field
+                          .slice(1)
+                          .split(/(?=[A-Z])/)
+                          .join(' ')}
+                    </label>
+                    <input
+                      type={field || 'text'}
+                      className='form-control'
+                      id={field}
+                      onChange={(e) => handleFormChange(e, field)}
+                      value={registerData[field]}
+                    />
+                  </div>
+                );
+              })}
             </form>
           </Modal.Body>
           <Modal.Footer>
@@ -200,9 +136,6 @@ const RegisterModal = ({ show, onClose, registered, setRegistered }) => {
             </Button>
             <Button variant='primary' onClick={register}>
               Register
-            </Button>
-            <Button variant='primary' onClick={registerForm}>
-              RegisterForm
             </Button>
             <Button variant='primary' onClick={clearFormFields}>
               Clear Form
