e9fa874b94e17f88910dc1b885b7255a4a3ff1c2
David Perelman <dperelman3@gmail.com>
2022-06-08 21:15:55 +0300

    clearError and try AuthService.login

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -3,10 +3,16 @@ import LoginModal from './LoginModal';
 import Modal from './Modal';
 import Button from './Button';
 import Form from './Form';
+import {
+  isFormFieldsValid,
+  isValidPassword,
+  isValidEmail,
+} from '../utils/formValidation';
 import AuthService from '../services/AuthServices';
 
 const LoginButton = ({ setLoggedIn }) => {
   const [show, setShow] = useState(false);
+  const [error, setError] = useState('');
 
   const [loginData, setLoginData] = useState({
     email: '',
@@ -35,7 +41,37 @@ const LoginButton = ({ setLoggedIn }) => {
   };
 
   const login = () => {
-    AuthService.login(loginData);
+    if (
+      isFormFieldsValid(
+        loginData.firstName,
+        loginData.lastName,
+        loginData.email,
+        loginData.password,
+        loginData.verifyPassword
+      )
+    ) {
+      setError('all fields required!');
+    } else if (isValidPassword(loginData.password, loginData.verifyPassword)) {
+      setError(
+        'Password must be at least 6 characters long and the passwords must be identical'
+      );
+    } else if (!isValidEmail(loginData.email)) {
+      setError('invalid email!');
+    } else {
+      setError('');
+    }
+    try {
+      AuthService.login(loginData);
+      clearFormFields();
+    } catch (err) {
+      console.log(err.response.data.errMessage);
+      setError(err.response.data.errMessage);
+    }
+
+    const clearError = setTimeout(clearErrorMessage, 3000);
+    function clearErrorMessage() {
+      setError('');
+    }
   };
 
   return (
@@ -50,7 +86,13 @@ const LoginButton = ({ setLoggedIn }) => {
         Login
       </Button>
       {show && (
-        <Modal show={show} onClose={handleClose} title='Login' onSubmit={login}>
+        <Modal
+          show={show}
+          onClose={handleClose}
+          title='Login'
+          onSubmit={login}
+          error={error}
+        >
           <Form data={loginData} handleFormChange={handleFormChange}></Form>
         </Modal>
       )}
