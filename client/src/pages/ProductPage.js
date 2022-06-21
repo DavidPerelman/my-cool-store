88b88022e9c9477ea401d7c06f5cb6107c281294
David Perelman <dperelman3@gmail.com>
2022-06-21 18:50:18 +0300

    ProductPage styling

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import Button from '../components/Button/Button';
 import './ProductPage.css';
 
 const ProductPage = (props) => {
@@ -13,7 +14,10 @@ const ProductPage = (props) => {
     <div className='ProductPage'>
       <div className='product-div'>
         <div className='product-content'>
-          <p>
+          <h1>{state.title}</h1>
+          <p>{state.description}</p>
+          <h3>{state.price}$</h3>
+          {/* <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -21,7 +25,8 @@ const ProductPage = (props) => {
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          </p> */}
+          <Button>Add to cart</Button>
         </div>
         <div className='product-image-div'>
           <img src={state.images[0]} className='product-image' />
