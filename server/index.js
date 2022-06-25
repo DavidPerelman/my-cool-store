4ba8e289465edd9511b61107a9aa07403d00c1b2
David Perelman <dperelman3@gmail.com>
2022-06-26 00:22:28 +0300

    app.use(cors(corsOptions));

--- server/index.js
+++ server/index.js
@@ -6,7 +6,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-app.use(cors());
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 dotenv.config();
 
