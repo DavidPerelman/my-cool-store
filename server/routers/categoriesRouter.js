547e032663fd70877fd9767ab7ffc4563101920b
David Perelman <dperelman3@gmail.com>
2022-06-26 15:52:27 +0300

    categoriesRouter setup

--- server/routers/categoriesRouter.js
+++ server/routers/categoriesRouter.js
@@ -1,24 +1,24 @@
 const router = require('express').Router();
-const Product = require('../models/productModel');
+const Category = require('../models/categoryModel');
 const fs = require('fs');
 
-router.get('/products', async (req, res) => {
+router.get('/categories', async (req, res) => {
   try {
-    console.log('products');
+    console.log('categories');
   } catch (err) {
     console.error(err);
     res.status(500).send();
   }
 });
 
-router.post('/products/createProduct', async (req, res) => {
+router.post('/categories/createCategory', async (req, res) => {
   try {
-    // const data = { title, price, description, category, images } = req.body;
+    // const data = { name } = req.body;
     let rawdata = fs.readFileSync('./data.json');
     let product = JSON.parse(rawdata);
     let newItems = [];
 
-    console.log('products');
+    console.log('categories');
   } catch (err) {
     console.error(err);
     res.status(500).send();
