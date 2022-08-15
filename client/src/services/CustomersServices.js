80d5dc549671810324d0ba5f085bb659c3254838
David Perelman <dperelman3@gmail.com>
2022-08-15 15:13:44 +0300

    setup CustomersServices

---
+++ client/src/services/CustomersServices.js
@@ -0,0 +1,70 @@
+const CustomersServices = {
+  fetchAllCustomers: () => {
+    return fetch(`${process.env.REACT_APP_API_URL}/customers/customers`).then(
+      (res) => {
+        if (res.status !== 401) {
+          return res.json().then((data) => data);
+        } else {
+          return { message: 'error' };
+        }
+      }
+    );
+  },
+  fetchAllProductsByCategory: (categoryId, page, limit) => {
+    return fetch(
+      `${process.env.REACT_APP_API_URL}/categories/category/${categoryId}/products?page=${page}&limit=${limit}`
+    ).then((res) => {
+      if (res.status !== 401) {
+        return res.json().then((data) => data.results);
+      } else {
+        return { message: 'error' };
+      }
+    });
+  },
+  fetchAllProductsByCategoryId: (categoryId) => {
+    return fetch(
+      `${process.env.REACT_APP_API_URL}/categories/category/${categoryId}/categoryProducts`
+    ).then((res) => {
+      if (res.status !== 401) {
+        return res.json().then((data) => data.products);
+      } else {
+        return { message: 'error' };
+      }
+    });
+  },
+  fetchProduct: (productId) => {
+    return fetch(
+      `${process.env.REACT_APP_API_URL}/products/product/${productId}`
+    ).then((res) => {
+      if (res.status !== 401) {
+        return res.json().then((data) => data);
+      } else {
+        return { message: 'error' };
+      }
+    });
+  },
+  fetchCategoriesData: () => {
+    return fetch(`${process.env.REACT_APP_API_URL}/categories/categories`).then(
+      (res) => {
+        if (res.status !== 401) {
+          return res.json().then((data) => data);
+        } else {
+          return { message: 'error' };
+        }
+      }
+    );
+  },
+  fetchCategoryData: (categoryId) => {
+    return fetch(
+      `${process.env.REACT_APP_API_URL}/categories/categories/${categoryId}`
+    ).then((res) => {
+      if (res.status !== 401) {
+        return res.json().then((data) => data);
+      } else {
+        return { message: 'error' };
+      }
+    });
+  },
+};
+
+export default CustomersServices;
---
+++ server/routers/customersRouter.js
@@ -0,0 +1,77 @@
+const router = require('express').Router();
+const User = require('../models/userModel');
+const fs = require('fs');
+
+router.get('/customers', async (req, res) => {
+  try {
+    // get all customers
+    const customers = await User.find({});
+    res.json({ customers: customers });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+router.get('/product/:productId', async (req, res) => {
+  try {
+    const productId = req.params.productId;
+
+    // get single product
+    const product = await Product.findById(productId).exec();
+
+    res.json({ product: product });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+router.post('/createProduct', async (req, res) => {
+  try {
+    fs.readFile('../server/productsData.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+router.put('/editProduct', async (req, res) => {
+  try {
+    const fileData = fs.readFileSync('../server/productsData.json', 'utf8');
+
+    const jsonData = JSON.parse(fileData);
+
+    // jsonData['survival'] = 'online';
+
+    jsonData.forEach(function (v) {
+      delete v.id;
+      v.category = v.category['name'];
+    });
+
+    const newJsonData = JSON.stringify(jsonData);
+
+    fs.writeFile(
+      '../server/newProductsData.json',
+      newJsonData,
+      'utf8',
+      function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          //Everything went OK!
+        }
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
