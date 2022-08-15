5982b7356502af40b9d3924790c13d66eccd4b25
David Perelman <dperelman3@gmail.com>
2022-08-15 15:10:34 +0300

    setup expands th in ManageOrders

--- client/src/admin/pages/ManageOrders/ManageOrders.js
+++ client/src/admin/pages/ManageOrders/ManageOrders.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../../components/Button/Button';
 import MyTable from '../../../components/MyTable/MyTable';
+import Popover from '../../../components/Popover/Popover';
 import OrdersServices from '../../../services/OrdersServices';
 import './ManageOrders.css';
+import ExpandIcon from '../../asset/expand-icon.png';
 
 const ManageOrders = () => {
   const navigate = useNavigate();
@@ -26,11 +28,70 @@ const ManageOrders = () => {
     // navigate(`/order/${orderId}`);
   };
 
+  const expand = async (key) => {
+    // const content = document.getElementById(`${key}-content`);
+    // const allContents = document.getElementsByClassName(`content`);
+    // for (let i = 0; i < allContents.length; i++) {
+    //   allContents[i].style.visibility = 'hidden';
+    // }
+    // if (!visibile) {
+    //   content.style.visibility = 'visible';
+    //   setVisibile(true);
+    // } else {
+    //   content.style.visibility = 'hidden';
+    //   setVisibile(false);
+    // }
+    // content.style.visibility = 'visible';
+    // setVisibile(true);
+  };
+
+  const categoryListClick = async (categoryId) => {
+    // console.log(categoryId);
+    // ProductsServices.fetchAllProductsByCategoryId(categoryId).then((data) => {
+    //   setProducts(data);
+    //   setTableData(data);
+    //   setPaginatedProducts(data.slice(0, 10));
+    // });
+  };
+
   const renderHeader = () => {
     let headerData = ['No.', 'Order Number', 'Customer', 'Date', 'Status'];
+    let data = ['No.', 'Order Number', 'Customer', 'Date', 'Status'];
 
     return headerData.map((key, i) => {
-      return <th key={i}>{key}</th>;
+      console.log(data[i]);
+      // return <th key={i}>{key}</th>;
+      return (
+        (key !== 'No.' && (
+          <th key={i} id={`${key}-th`}>
+            <Popover
+              icon={ExpandIcon}
+              keyValue={key}
+              onClick={() => expand(key)}
+              data={data[i]}
+              categoryListClick={categoryListClick}
+            >
+              <ul>
+                {data &&
+                  data.map((category, i) => (
+                    <li
+                      key={i}
+                      id={category._id}
+                      onClick={() => categoryListClick(category._id)}
+                    >
+                      {category.name}
+                    </li>
+                  ))}
+              </ul>
+            </Popover>
+            {key}
+          </th>
+        )) || (
+          <th key={i} id={`${key}-th`}>
+            {key}
+          </th>
+        )
+      );
     });
   };
 
--- client/src/components/Popover/Popover.js
+++ client/src/components/Popover/Popover.js
@@ -5,6 +5,7 @@ const Popover = ({ keyValue, icon, children, onClick }) => {
   return (
     <div className='wrapper' id={`wrapper-${keyValue}`}>
       <img
+        style={{ width: '20px', cursor: 'pointer', border: '1px black solid' }}
         id={`${keyValue}-popover-icon`}
         src={icon}
         alt='icon'
